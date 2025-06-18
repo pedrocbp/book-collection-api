e09b86ed9a96c5d57e7dbf59ee168262206e168c
Pedro <pedromiguelboce@gmail.com>
2025-06-17 21:49:30 -0300

    feat: implement middleware to check if session ID exists and validate user

---
+++ middlewares/check-session-id-exists.ts
@@ -0,0 +1,21 @@
+import { knex } from '@/database'
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function checkSessionIdExists(
+  request: FastifyRequest,
+  response: FastifyReply,
+) {
+  const sessionId = request.cookies.sessionId
+
+  if (!sessionId) {
+    return response.status(401).send({ error: 'Unauthorized' })
+  }
+
+  const user = await knex('users').where({ session_id: sessionId }).first()
+
+  if (!user) {
+    return response.status(401).send({ error: 'Unauthorized' })
+  }
+
+  request.user = user
+}

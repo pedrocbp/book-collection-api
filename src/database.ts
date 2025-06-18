cd97d0e427004785fb9d795d460e953d79c5c6da
Pedro <pedromiguelboce@gmail.com>
2025-06-17 21:39:53 -0300

    feat: add database configuration and knex setup

---
+++ src/database.ts
@@ -0,0 +1,15 @@
+import { knex as setupKnex } from 'knex'
+
+export const config = {
+  client: 'sqlite',
+  connection: {
+    filename: './db/app.db',
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = setupKnex(config)

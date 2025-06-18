e9064dc0e32a096d0105038380c3dac5c020ee91
Pedro <pedromiguelboce@gmail.com>
2025-06-17 21:48:08 -0300

    feat: add type definitions for knex tables including user and books

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,29 @@
+// eslint-disable-next-line
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    user: {
+      id: string
+      session_id?: string
+      name: string
+      last_name: string
+      email: string
+      created_at: string
+      updated_at: string
+    }
+    books: {
+      id: string
+      user_id?: string
+      title: string
+      genre: string
+      author: string
+      publication_year: number
+      status: string
+      rating: number
+      review: string
+      created_at: string
+      updated_at: string
+    }
+  }
+}
